76e39ad2adbb267aa4262a7d09b8cea1ab75a07b
AndresKorvin <andreskorvin@gmail.com>
2023-02-12 13:07:08 +0300

    Add Some-, One-, ZeroSearchTicket

--- src/test/java/org/example/ManagerTest.java
+++ src/test/java/org/example/ManagerTest.java
@@ -5,15 +5,16 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class ManagerTest {
-    Ticket ticket1 = new Ticket(1, 600, "London", "Paris", 2);
-    Ticket ticket2 = new Ticket(2, 800, "Prague", "Dublin", 4);
-    Ticket ticket3 = new Ticket(3, 900, "Prague", "Dublin", 4);
-    Ticket ticket4 = new Ticket(4, 500, "London", "Prague", 3);
-    Ticket ticket5 = new Ticket(5, 400, "Paris", "Dublin", 1);
-    Ticket ticket6 = new Ticket(6, 300, "London", "Paris", 2);
-    Ticket[] tickets = new Ticket[]{ticket1, ticket2, ticket3, ticket4, ticket5, ticket6};
-    Repository repository = new Repository(tickets);
-    Manager manager = new Manager(repository);
+    Ticket ticket1 = new Ticket(1, 600, "London", "Paris", 61);
+    Ticket ticket2 = new Ticket(2, 800, "Prague", "Dublin", 85);
+    Ticket ticket3 = new Ticket(3, 900, "Prague", "Dublin", 96);
+    Ticket ticket4 = new Ticket(4, 500, "London", "Paris", 38);//
+    Ticket ticket5 = new Ticket(5, 400, "Paris", "Dublin", 55);
+    Ticket ticket6 = new Ticket(6, 300, "London", "Paris", 73);//
+    Ticket ticket7 = new Ticket(7, 350, "London", "Dublin", 102);
+    Ticket ticket8 = new Ticket(8, 450, "London", "Paris", 95);//
+    Ticket ticket9 = new Ticket(9, 650, "London", "Paris", 81);
+    Ticket ticket10 = new Ticket(10, 750, "London", "Prague", 68);
 
     @ParameterizedTest
     @CsvSource({
@@ -24,9 +25,57 @@ class ManagerTest {
             "London, ris",
             "don, ris"
     })
-    void search(String from, String to) {
+    void search6Tickets(String from, String to) {
+
+        Ticket[] tickets = new Ticket[]{ticket1, ticket2, ticket3, ticket4, ticket5, ticket6, ticket7, ticket8, ticket9, ticket10};
+        Repository repository = new Repository(tickets);
+        Manager manager = new Manager(repository);
+
         Ticket[] actual = manager.search(from, to);
-        Ticket[] expected = {ticket6, ticket1};
+        Ticket[] expected = {ticket6, ticket8, ticket4, ticket1, ticket9};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "London, Paris",
+            "Lon, Paris",
+            "London, Par",
+            "don, Paris",
+            "London, ris",
+            "don, ris"
+    })
+    void search1Ticket(String from, String to) {
+
+        Ticket[] tickets = new Ticket[]{ticket2, ticket3, ticket5, ticket6, ticket7, ticket10};
+        Repository repository = new Repository(tickets);
+        Manager manager = new Manager(repository);
+
+        Ticket[] actual = manager.search(from, to);
+        Ticket[] expected = {ticket6};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "London, Paris",
+            "Lon, Paris",
+            "London, Par",
+            "don, Paris",
+            "London, ris",
+            "don, ris"
+    })
+    void search0Tickets(String from, String to) {
+
+        Ticket[] tickets = new Ticket[]{ticket2, ticket3, ticket5, ticket7, ticket10};
+        Repository repository = new Repository(tickets);
+        Manager manager = new Manager(repository);
+
+        Ticket[] actual = manager.search(from, to);
+        Ticket[] expected = {};
+
         Assertions.assertArrayEquals(expected, actual);
     }
 }
